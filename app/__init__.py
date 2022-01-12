96e981215667138b77a6c26e4c9dfee5a961bf1e
theharveyminingco <theharveyminingco@gmail.com>
2022-01-12 09:29:09 -0600

    functional basic template

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, app
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path,'app.sqlite')
     )
 
@@ -16,8 +16,8 @@ def create_app(test_config=None):
 
     try:
         os.makedirs(app.instance_path)
-        except OSError:
-            pass
+    except OSError:
+        pass
     
     @app.route('/hello')
     def hello():
